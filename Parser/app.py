417a4b400045ecd3813c28e773a7913c0befc117
Alona Mov <alonamov@gmail.com>
2020-04-21 11:28:15 +0300

    Dump parser will read from db.db and savev pickled HttpConvarsation object (not parsing headers for now)

--- Parser/app.py
+++ Parser/app.py
@@ -1,5 +1,6 @@
 import sqlite3
 import regex_founder
+import pickle
 from SharedUtils.HttpClass.HttpConversation import HttpConversation
 from SharedUtils.HttpClass.Packet import Packet
 from SharedUtils.HttpClass.HttpBody import HttpBody
@@ -8,18 +9,23 @@ from SharedUtils.HttpClass.HttpHeaders import HttpHeaders
 def main():
     conn = sqlite3.connect('db.db')
     cur = conn.cursor()
-    list_conversations = list()
+    cur.execute("CREATE TABLE IF NOT EXISTS ParsedConversations (id INTEGER PRIMARY KEY AUTOINCREMENT, api TEXT, method TEXT, conversation BLOB)")
     cur.execute("select * from RawConversations")
     conversations =  cur.fetchall()
 
     for conversation in conversations:
-        fixed_api = regex_founder.get_regex_of_url(convarsation[1])
+        fixed_api = regex_founder.get_regex_of_url(conversation[1].split('://')[1])
         req = Packet()
         req.http_body = HttpBody('JSON', conversation[4])
         res = Packet()
         res.http_body = HttpBody('JSON', conversation[6])
         cur_conversation = HttpConversation(req, res, fixed_api, conversation[2])
+        cur.execute("INSERT INTO ParsedConversations (api, method, conversation) VALUES (?, ?, ?)", (fixed_api, conversation[2], pickle.dumps(cur_conversation, 0)))
         #parse headers
+
+    cur.close()
+    conn.commit()
+    conn.close()
 
 
 if __name__ == "__main__":
---
+++ Parser/optional_url_regex.py
@@ -0,0 +1,16 @@
+import re
+
+# Here stored all the regex for urls, its important not add here regex that you not sure that need be here.
+# The order of the regex is important, because in code he start from the beginning and want found the must explicit.
+OPTIONAL_URL_REGEX_STRINGS = [
+    r'^([0-9a-f]){8}-([0-9a-f]){4}-([0-9a-f]){4}-([0-9a-f]){4}-([0-9a-f]){12}$',
+    r'^[a-f0-9]{32}$',
+    r'^v2$',
+    r'^v3$',
+    r'^v2.0$',
+    r'^v2.1$',
+    r'^([0-9a-f]){8}-([0-9a-f]){4}-([0-9a-f]){4}-([0-9a-f]){4}-([0-9a-f]){12}.json$',
+    r'^[a-f0-9]{32}.json$'
+]
+
+OPTIONAL_URL_REGEX = [re.compile(s) for s in OPTIONAL_URL_REGEX_STRINGS]
