c9d5da1c4cc3bd7a854ce38a6a17af7fd1831fce
Alona movshovich <alonamovshovich@Alonas-MacBook-Pro.local>
2020-04-28 12:40:46 +0300

    now its less stupid

--- run_schemerator.py
+++ run_schemerator.py
@@ -1,8 +1,8 @@
 import os
 import subprocess
+from argparse import ArgumentParser
 
 MODULES = ['Proxy', 'Parser', 'Generator', 'WebExport']
-
 
 def build_image(image_name, dockerfile_folder):
     if subprocess.Popen('docker build -t {} -f {}/Dockerfile .'.format(image_name, dockerfile_folder), shell=True).wait() != 0:
@@ -12,24 +12,34 @@ def run_build():
     for module in MODULES:
         build_image(module.lower() + '_img', module)
 
-def run_schemerator():
+def run_schemerator(work_dir):
     try:
         print('you have 3 minutes to interact with the proxy in localhost:8080')
-        subprocess.Popen('docker run -p 8080:8080 -v /Users/alonamovshovich/Documents/Work/Schemerator/schemerator/db:/home/mitmproxy/db -e APP_URL=172.20.10.2 -e APP_PORT=8082 --name proxy proxy_img', shell=True).wait(180)
+        subprocess.Popen('docker run -p 8080:8080 -v {}/db:/home/mitmproxy/db -e APP_URL=172.20.10.2 -e APP_PORT=8082 --name proxy proxy_img'.format(work_dir), shell=True).wait(180)
     except Exception as e:
         subprocess.Popen('docker stop proxy', shell=True)
         print("finished proxy")
 
     for module in MODULES[1:-1]:
-        subprocess.Popen('docker run -v /Users/alonamovshovich/Documents/Work/Schemerator/schemerator/db:/home/db {}'.format(module.lower() + '_img'), shell=True).wait()
+        subprocess.Popen('docker run -v {}/db:/home/db {}'.format(work_dir,module.lower() + '_img'), shell=True).wait()
 
-    subprocess.Popen('docker run -p 8080:8080 -v /Users/alonamovshovich/Documents/Work/Schemerator/schemerator/db:/home/db webexport_img', shell=True).wait()
+    subprocess.Popen('docker run -p 8080:8080 -v {}/db:/home/db webexport_img'.format(work_dir), shell=True).wait()
     
 
 def main():
-    os.chdir('/Users/alonamovshovich/Documents/Work/Schemerator/schemerator/')
-    run_build()
-    run_schemerator()
+    pars = ArgumentParser()
+    pars.add_argument('-b', action='store_true', default=False)
+    pars.add_argument('-r', action='store_true', default=False)
+    
+    args = pars.parse_args()
+    work_dir = os.getcwd()
+
+    if args.b:
+        print("building docker images...")
+        run_build()
+    if args.r:
+        print("running schemerator...")
+        run_schemerator(work_dir)
 
 if __name__ == "__main__":
     main()
