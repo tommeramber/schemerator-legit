0a502d6aac4e12aacc0b506db65169b0050e8032
Alona movshovich <alonamovshovich@Alonas-MacBook-Pro.local>
2020-05-03 16:18:50 +0300

    Added init function for main_logger, should be could before using the global logger

--- Generator/Utils/loggers/main_logger.py
+++ Generator/Utils/loggers/main_logger.py
@@ -1,21 +1,24 @@
 import logging
 import sys
-import pathlib2
+from pathlib import Path
 
-##################################################################
-# Log config.
-main_logger = logging.getLogger("main_logger")
-main_logger.setLevel(logging.INFO)
-formatter = logging.Formatter("%(asctime)s:%(name)s:%(message)s")
+logger = None
 
-# add file handler
-pathlib2.Path("logs").mkdir(exist_ok=True)
-log_file_handler = logging.FileHandler("logs/main.log", mode="a")
-log_file_handler.setFormatter(formatter)
-main_logger.addHandler(log_file_handler)
-# add stdout handler
-stdout_handler = logging.StreamHandler(sys.stdout)
-stdout_handler.setFormatter(formatter)
-stdout_handler.setLevel(logging.INFO)
-main_logger.addHandler(stdout_handler)
-##################################################################
+
+def initialize_logger(log_name):
+    # Log config.
+    global main_logger
+    main_logger = logging.getLogger(log_name)
+    formatter = logging.Formatter("%(asctime)s (%(pathname)s:%(lineno)d) - %(levelname)s: %(message)s")
+
+    # add file handler
+    Path("logs").mkdir(exist_ok=True)
+    log_file_handler = logging.FileHandler("logs/{}.log".format(log_name), mode="a")
+    log_file_handler.setFormatter(formatter)
+    log_file_handler.setLevel(logging.DEBUG)
+    main_logger.addHandler(log_file_handler)
+    # add stdout handler
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setFormatter(formatter)
+    stdout_handler.setLevel(logging.DEBUG)
+    main_logger.addHandler(stdout_handler)
